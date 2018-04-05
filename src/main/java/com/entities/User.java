61b463e5c222ca7f174e1a69a9dc19dd5634f9d3
DanMihailescu <as42554525@yahoo.ca>
2018-04-05 03:04:46 -0400

    Revert "TestCases"

--- src/main/java/com/entities/User.java
+++ src/main/java/com/entities/User.java
@@ -147,11 +147,6 @@ public class User {
     public void follow(User other){
         followedUsers.add(other);
     }
-    
-    public void unfollow(User other)
-    {
-    	this.followedUsers.remove(other);
-    }
 
 	public Long getId() {
 		return id;
