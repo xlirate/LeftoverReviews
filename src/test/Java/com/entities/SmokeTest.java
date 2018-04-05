3e29623c52627374d9f0346a16cb4ecfc7411798
DanMihailescu <as42554525@yahoo.ca>
2018-04-05 01:22:25 -0400

    SmokeTest Fixed

--- src/test/Java/com/entities/SmokeTest.java
+++ src/test/Java/com/entities/SmokeTest.java
@@ -2,7 +2,7 @@ package com.entities;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import com.controller.ReviewSystemController;
+import com.controller.*;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,10 +14,38 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class SmokeTest {
 
     @Autowired
-    private ReviewSystemController controller;
+    private AccountController accountCon;
+    @Autowired
+    private ProductController productCon;
+    @Autowired
+    private ReviewRestController reviewRestCon;
+    @Autowired
+    private UsersController usersCon;
+    @Autowired
+    private ViewController viewCon;
 
     @Test
-    public void contextLoads() throws Exception {
-        assertThat(controller).isNotNull();
+    public void accountControllerContextLoads() throws Exception {
+        assertThat(accountCon).isNotNull();
+    }
+
+    @Test
+    public void productControllerContextLoads() throws Exception {
+        assertThat(productCon).isNotNull();
+    }
+
+    @Test
+    public void reviewRestControllerContextLoads() throws Exception {
+        assertThat(reviewRestCon).isNotNull();
+    }
+
+    @Test
+    public void usersControllerContextLoads() throws Exception {
+        assertThat(usersCon).isNotNull();
+    }
+
+    @Test
+    public void viewControllerContextLoads() throws Exception {
+        assertThat(viewCon).isNotNull();
     }
 }
